e63fdc75032492961971e3e164834823a43558ce
lokanath99 <lokanath1912@gmail.com>
2024-11-05 17:53:17 +0530

    05/11

--- Dart/fun.dart
+++ Dart/fun.dart
@@ -5,28 +5,62 @@ void main(){
   // }
   
 
-  String printFunc([String statement = 'Don joe']){
-    statement += " hello this is you statement";
-    return statement;
-  }
+  // String printFunc([String statement = 'Don joe']){
+  //   statement += " hello this is you statement";
+  //   return statement;
+  // }
 
-  var str = printFunc("Lokanath");
-  print(str);
+  // var str = printFunc("Lokanath");
+  // print(str);
 
 
-  int add(int a, int b){
-    print(a);
-    print(b);
-    return a+b;
-  }
+  // int add(int a, int b, [int? c]){
+  //   print(a);
+  //   print(b);
+  //   print(c);
+  //   return a+b;
+  // }
 
-  print(add(1, 5));
+  // print(add(1, 5, 7));
 
-  double? sum({double? a, double? b }){
-    print(a);
-    print(b);
-    return a;
-  };
+  // double? sum({double? a, double? b}){
+  //   print(a);
+  //   print(b);
+  //   return a;
+  // };
 
-  print(sum(b:5.6, a:5.6));
+  // print(sum(b:5.6, a:5.6));
+
+
+  // void printInfo({required String name,required String gender}){
+  //   print("My name is ${name} and my gender is ${gender}");
+  // }
+  
+  // printInfo(name: "Lokanath", gender: "male");
+  
+
+  // "asldfkja"
+  // void printInfo({required String name,required String gender}){
+  //   print("My name is ${name} and my gender is ${gender}");
+  // }
+
+  // var str = "asdfasdf";
+  // var func = (){
+  //     print("hello from anonymous function");
+  //     return 5;
+  // };
+
+  // List<int> lst = [1,2,3,6,5,4,7,8,9];
+  // lst.forEach( (asdf){
+  //   print(asdf);
+  // });
+
+  // func();
+
+  var func2 = () => print("hello this is a arrow function");
+  var b;
+  var even = (a) => b =a+=2; 
+  func2();
+  even(5);
+  print(b);
 }
